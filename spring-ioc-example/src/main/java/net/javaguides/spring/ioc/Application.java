621142c525f440f218a0253f6a08bf33a9644de1
pradeepkumaresan <pradeepk@thoughtworks.com>
2023-06-20 23:07:41 +0530

    add git ignore

--- spring-ioc-example/src/main/java/net/javaguides/spring/ioc/Application.java
+++ spring-ioc-example/src/main/java/net/javaguides/spring/ioc/Application.java
@@ -1,13 +1,14 @@
 package net.javaguides.spring.ioc;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
 	public static void main(String[] args) {
-		// ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		AnnotationConfigApplicationContext  context = new AnnotationConfigApplicationContext(AppConfig.class);
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		//ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
 		obj.getMessage();
-		context.close();
 	}
 }
