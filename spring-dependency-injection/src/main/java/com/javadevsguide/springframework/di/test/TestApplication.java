52a8461e04a3e4dbb02eae23131d747368a38e1a
pradeepkumaresan <pradeepk@thoughtworks.com>
2023-06-25 16:04:59 +0530

    add constructor and setter based injection examples

--- spring-dependency-injection/src/main/java/com/javadevsguide/springframework/di/test/TestApplication.java
+++ spring-dependency-injection/src/main/java/com/javadevsguide/springframework/di/test/TestApplication.java
@@ -1,5 +1,7 @@
 package com.javadevsguide.springframework.di.test;
 
+import com.javadevsguide.springframework.di.constructor.ConstructorBasedInjection;
+import com.javadevsguide.springframework.di.setter.SetterBasedInjection;
 import org.springframework.context.ApplicationContext;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -10,7 +12,13 @@ import com.javadevsguide.springframework.di.field.FieldBasedInjection;
 public class TestApplication {
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
-		FieldBasedInjection  fieldBasedInjection = applicationContext.getBean(FieldBasedInjection.class);
-		fieldBasedInjection.processMsg("twitter message sending ");
+		FieldBasedInjection fieldBasedInjection = applicationContext.getBean(FieldBasedInjection.class);
+		fieldBasedInjection.processMsg("message sent from field injection");
+
+		SetterBasedInjection setterBasedInjection = applicationContext.getBean(SetterBasedInjection.class);
+		setterBasedInjection.processMsg("message sent from setter injection");
+
+		ConstructorBasedInjection constructorBasedInjection = applicationContext.getBean(ConstructorBasedInjection.class);
+		constructorBasedInjection.processMsg("message sent from constructor injection");
 	}
 }
